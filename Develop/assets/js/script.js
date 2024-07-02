3640667ae56bc80f6cdcab6a9851d15a53995e92
Charlie <charlieboyle252@gmail.com>
2024-07-02 15:03:26 -0500

    addition of new task card creation function, addition of reading data from modal form function, implementation of day.js in html

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -8,7 +8,7 @@ const titleInput = $("#task-input")
 const dateInput = $("#datepicker")
 const descriptionInput = $("#description-input")
 
-function readProjectsFromStorage() {
+function readTasksFromStorage() {
   let tasks = JSON.parse(localStorage.getItem("tasks"));
 
   if (!tasks) {
@@ -24,17 +24,42 @@ function saveTasksToStorage(newTask) {
 }
 
 function createTaskCard(task) {
+    const taskCard = $('<div>')
+      .addClass('card project-card draggable my-3')
+      .attr('data-project-id', task.id);
+    const cardHeader = $('<div>').addClass('card-header h4').text(task.name);
+    const cardBody = $('<div>').addClass('card-body');
+    const cardDescription = $('<p>').addClass('card-text').text(task.type);
+    const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+    const cardDeleteBtn = $('<button>')
+      .addClass('btn btn-danger delete')
+      .text('Delete')
+      .attr('data-project-id', task.id);
+    cardDeleteBtn.on('click', handleDeleteTask);
+
+    if (task.dueDate && task.status !== 'done') {
+      const now = dayjs();
+      const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
   
-  var dynamicClass = ""
-  var cardHTML = `<div class="card ${dynamicClass}"> 
-  <h1>${task.title}</h1>
-  <h2>Due on ${task.dueDate}</h2>
-  </div>`
-  return cardHTML
+      // ? If the task is due today, make the card yellow. If it is overdue, make it red.
+      if (now.isSame(taskDueDate, 'day')) {
+        taskCard.addClass('bg-warning text-white');
+      } else if (now.isAfter(taskDueDate)) {
+        taskCard.addClass('bg-danger text-white');
+        cardDeleteBtn.addClass('border-light');
+      }
+    }
+  
+    // ? Gather all the elements created above and append them to the correct elements.
+    cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+    taskCard.append(cardHeader, cardBody);
+  
+    // ? Return the card so it can be appended to the correct lane.
+    return taskCard;
+
 }
 
 function renderTaskList() {
-  
   if (taskList === null) {
     console.log("TaskList didn't exist!!")
     taskList = []
@@ -45,21 +70,39 @@ function renderTaskList() {
   for (let i = 0; i < taskList.length; i++) {
     const task = taskList[i];
 
+
   }
 }
 
 function handleAddTask(event) {
   event.preventDefault()
-  // When a User Submits a Form
 
-  // 1. Validate the form
+  const taskName = taskNameInputEl.val().trim();
+  const taskType = taskTypeInputEl.val(); // don't need to trim select input
+  const taskDate = taskDateInputEl.val(); // yyyy-mm-dd format
 
-  // 2. If invalid {alert("error")  return}
+  const newTask = {
+    // ? Here we use a Web API called `crypto` to generate a random id for our project. This is a unique identifier that we can use to find the project in the array. `crypto` is a built-in module that we can use in the browser and Nodejs.    id: crypto.randomUUID(),
+    name: taskName,
+    type: taskType,
+    dueDate: taskDate,
+    status: 'to-do',
+  };
 
-  // 3. It's valid -> newTask = {title, description, due-date, progress: "to-do"}
-  // saveTasksToStorage(newTask)
-  // clear my inputs again -> new form
-  // element.empty()
+  // ? Pull the projects from localStorage and push the new project to the array
+  const tasks = readTasksFromStorage();
+  tasks.push(newProject);
+
+  // ? Save the updated projects array to localStorage
+  saveProjectsToStorage(projects);
+
+  // ? Print project data back to the screen
+  printProjectData();
+
+  // ? Clear the form inputs
+  taskNameInputEl.val('');
+  taskTypeInputEl.val('');
+  taskDateInputEl.val('');
 }
 
 function handleDeleteTask(event) {
