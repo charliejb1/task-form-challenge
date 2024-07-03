0c93fc6ff7ba5e1a228d76ec2f79ec6b061d324b
Charlie <charlieboyle252@gmail.com>
2024-07-03 14:56:52 -0500

    fixing of renderTaskList to empty project cards out of lanes, also adding cards as global variables

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -7,6 +7,9 @@ const taskForm = $("#task-form")
 const titleInput = $("#task-input")
 const dateInput = $("#datepicker")
 const descriptionInput = $("#description-input")
+const doneCards = $("#done-cards")
+const inProgressCards = $("#in-progress-cards")
+const toDoCards = $("#todo-cards")
 
 function readTasksFromStorage() {
   let tasks = JSON.parse(localStorage.getItem("tasks"));
@@ -18,68 +21,80 @@ function readTasksFromStorage() {
 }
 
 function saveTasksToStorage(newTask) {
+  const taskList = readTasksFromStorage()
   taskList.push(newTask)
   localStorage.setItem("tasks", JSON.stringify(taskList))
   renderTaskList()
 }
 
 function createTaskCard(task) {
-    const taskCard = $('<div>')
-      .addClass('card project-card draggable my-3')
-      .attr('data-project-id', task.id);
-    const cardHeader = $('<div>').addClass('card-header h4').text(task.name);
-    const cardBody = $('<div>').addClass('card-body');
-    const cardDescription = $('<p>').addClass('card-text').text(task.type);
-    const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
-    const cardDeleteBtn = $('<button>')
-      .addClass('btn btn-danger delete')
-      .text('Delete')
-      .attr('data-project-id', task.id);
-    cardDeleteBtn.on('click', handleDeleteTask);
+  const taskCard = $('<div>')
+    .addClass('card project-card draggable my-3')
+    .attr('data-project-id', task.id);
+  const cardHeader = $('<div>').addClass('card-header h4').text(task.name);
+  const cardBody = $('<div>').addClass('card-body');
+  const cardDescription = $('<p>').addClass('card-text').text(task.type);
+  const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
+  const cardDeleteBtn = $('<button>')
+    .addClass('btn btn-danger delete')
+    .text('Delete')
+    .attr('data-project-id', task.id);
+  cardDeleteBtn.on('click', handleDeleteTask);
 
-    if (task.dueDate && task.status !== 'done') {
-      const now = dayjs();
-      const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
-  
-      // ? If the task is due today, make the card yellow. If it is overdue, make it red.
-      if (now.isSame(taskDueDate, 'day')) {
-        taskCard.addClass('bg-warning text-white');
-      } else if (now.isAfter(taskDueDate)) {
-        taskCard.addClass('bg-danger text-white');
-        cardDeleteBtn.addClass('border-light');
-      }
+  if (task.dueDate && task.status !== 'done') {
+    const now = dayjs();
+    const taskDueDate = dayjs(task.dueDate, 'DD/MM/YYYY');
+
+    // ? If the task is due today, make the card yellow. If it is overdue, make it red.
+    if (now.isSame(taskDueDate, 'day')) {
+      taskCard.addClass('bg-warning text-white');
+    } else if (now.isAfter(taskDueDate)) {
+      taskCard.addClass('bg-danger text-white');
+      cardDeleteBtn.addClass('border-light');
     }
-  
-    // ? Gather all the elements created above and append them to the correct elements.
-    cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
-    taskCard.append(cardHeader, cardBody);
-  
-    // ? Return the card so it can be appended to the correct lane.
-    return taskCard;
+  }
 
+  // ? Gather all the elements created above and append them to the correct elements.
+  cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+  taskCard.append(cardHeader, cardBody);
+
+  return taskCard;
 }
 
+
 function renderTaskList() {
-  if (taskList === null) {
-    console.log("TaskList didn't exist!!")
-    taskList = []
-    localStorage.setItem("tasks", JSON.stringify(taskList))
-    return
-  }
+
+  const taskList = readTasksFromStorage()
+   
+    const todoList = $('#todo-cards');
+    todoList.empty();
+  
+    const inProgressList = $('#in-progress-cards');
+    inProgressList.empty();
+  
+    const doneList = $('#done-cards');
+    doneList.empty();
 
   for (let i = 0; i < taskList.length; i++) {
     const task = taskList[i];
 
 
+    if (task.status === "done" && task.dueDate === "done") {
+      doneCards.append(task)
+    } else if (task.status === "to-do") {
+      toDoCards.append(createTaskCard(task))
+    } else if (task.status === 'in-progress') {
+      inProgressCards.append(createProjectCard(project));
+    }
   }
 }
 
 function handleAddTask(event) {
   event.preventDefault()
-
-  const taskName = taskNameInputEl.val().trim();
-  const taskType = taskTypeInputEl.val(); // don't need to trim select input
-  const taskDate = taskDateInputEl.val(); // yyyy-mm-dd format
+  console.log("hello")
+  const taskType = taskForm.val().trim();
+  const taskName = titleInput.val(); // don't need to trim select input
+  const taskDate = dateInput.val(); // yyyy-mm-dd format
 
   const newTask = {
     // ? Here we use a Web API called `crypto` to generate a random id for our project. This is a unique identifier that we can use to find the project in the array. `crypto` is a built-in module that we can use in the browser and Nodejs.    id: crypto.randomUUID(),
@@ -90,19 +105,17 @@ function handleAddTask(event) {
   };
 
   // ? Pull the projects from localStorage and push the new project to the array
-  const tasks = readTasksFromStorage();
-  tasks.push(newProject);
 
   // ? Save the updated projects array to localStorage
-  saveProjectsToStorage(projects);
+  saveTasksToStorage(newTask);
 
   // ? Print project data back to the screen
-  printProjectData();
+  renderTaskList();
 
   // ? Clear the form inputs
-  taskNameInputEl.val('');
-  taskTypeInputEl.val('');
-  taskDateInputEl.val('');
+  taskForm.val('');
+  titleInput.val('');
+  dateInput.val('');
 }
 
 function handleDeleteTask(event) {
